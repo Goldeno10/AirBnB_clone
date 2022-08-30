5bdfdc1765705d952b8fd3317cb7d588dfcc77b0
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-08-30 10:27:12 +0100

    Added more funtionalities to base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ import uuid
 class BaseModel:
     """This class 'BaseModel' defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-        """Initializer method"""
+        """ Initializer method """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
@@ -25,7 +25,7 @@ class BaseModel:
 
 
     def __str__(self):
-        """Return string representation of the object"""
+        """ Return string representation of the object """
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
@@ -33,7 +33,7 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Return dictionary representation of object"""
+        """ Return dictionary representation of object """
         dict_rep = self.__dict__
         dict_rep['__class__'] = str(type(self).__name__)
         dict_rep['created_at'] = self.created_at.isoformat()
