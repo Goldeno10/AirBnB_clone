ce82da390a9dfbe2d90a08a180de34ee863eeb6d
Goldeno10 <ibrahimmuhamad271@gmail.com>
2022-09-09 14:23:13 +0100

    ...

--- console.py
+++ console.py
@@ -43,7 +43,8 @@ class HBNBCommand(cmd.Cmd):
         return arg
 
     def do_create(self, line):
-        """Creates a new instance of BaseModel, saves it
+        """
+        Creates a new instance of Class, saves it
         (to the JSON file) and prints the id.
         Ex: $ create BaseModel
         """
@@ -68,7 +69,8 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def do_show(self, arg):
-        """Prints the string representation of an instance
+        """
+        Prints the string representation of an instance
         based on the class name and id.
         Ex: $ show BaseModel 1234-1234-1234.
         """
@@ -98,7 +100,8 @@ class HBNBCommand(cmd.Cmd):
                     print("** class doesn't exist **")
 
     def do_destroy(self, line):
-        """ Deletes an instance based on the class name
+        """
+        Deletes an instance based on the class name
         and id (save the change into the JSON file).
         Ex: $ destroy BaseModel 1234-1234-1234.
         """
@@ -130,7 +133,8 @@ class HBNBCommand(cmd.Cmd):
                     print("** class doesn't exist **")
 
     def do_all(self, line):
-        """Prints all string representation of all instances
+        """
+        Prints all string representation of all instances
         based or not on the class name.
         Ex: $ all BaseModel or $ all.
         """
@@ -158,7 +162,8 @@ class HBNBCommand(cmd.Cmd):
                     return
 
     def do_update(self, line):
-        """Updates an instance based on the class name and id by
+        """
+        Updates an instance based on the class name and id by
         adding or updating attribute (save the change into the JSON file).
         Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
         """
